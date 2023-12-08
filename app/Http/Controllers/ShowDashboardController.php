a49bb54ddb8b6a93420e4fd9b01a663da9f167a1
Seth Sharp <sesharp@outlook.com>
2023-12-08 10:55:35 +1000

    test

--- app/Http/Controllers/ShowDashboardController.php
+++ app/Http/Controllers/ShowDashboardController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Domain\Iam\Models\Role;
 use Inertia\Inertia;
 
 class ShowDashboardController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('Management/Dashboard');
+        return Inertia::render('Management/Dashboard', [
+            'roles' => Role::all()
+        ]);
     }
 }
--- resources/js/Pages/Management/Dashboard.vue
+++ resources/js/Pages/Management/Dashboard.vue
@@ -1,6 +1,10 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
 import { Head } from '@inertiajs/vue3'
+
+defineProps({
+    roles: Object,
+})
 </script>
 
 <template>
@@ -10,6 +14,10 @@ import { Head } from '@inertiajs/vue3'
         <div class="text-gray-900 text-xl">Dashboard</div>
         <div>This is where you can manage your online store</div>
 
-        <div class="grid grid-cols-6 mt-6">What to put here?</div>
+        <div class="grid grid-cols-6 mt-6">
+            <div v-for="role in roles">
+                {{ role.name }}
+            </div>
+        </div>
     </AuthenticatedLayout>
 </template>
